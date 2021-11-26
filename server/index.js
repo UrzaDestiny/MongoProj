f9d798494b0ce5689271a3dc6a2c66fc38613b37
pavel.levin <pavel.levin@finteco.io>
2021-11-26 21:06:28 +0300

    add postMessage schema and post controllers

--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use( bodyParser.json({ limit: "30mb", extended: true }));
 app.use( bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster.qwqon.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@sandbox.qwqon.mongodb.net/Sandbox?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
