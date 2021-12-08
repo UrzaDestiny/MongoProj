3e4afa27017e780c50fac346124f01438646cee7
pavel.levin <pavel.levin@finteco.io>
2021-12-08 22:54:25 +0300

    add styles, image, impliment redux basis

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import redusers from './reducers';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(redusers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
